631a32e0553e5d6b77819203595d9e446e89ecb2
Cristianunrc <cristiankhan23@gmail.com>
2024-01-19 10:05:06 -0300

    Established Server-Client connection

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,23 @@
-io()
+// web socket Client connection
+
+$(function () {
+  
+  const socket = io()
+
+  // obtaining DOM elements (id's) from the index.html
+  const $messageForm = $('#message-form')
+  const $message = $('#message')
+  const $chat = $('#chat')
+
+  // events
+  $messageForm.submit( e => {
+    e.preventDefault()
+    socket.emit('send message', $message.val())
+    $message.val('')
+  })
+
+  socket.on('new message', function (data) {
+    $chat.append(data + '<br/>')
+  })
+
+})
--- src/sockets.js
+++ src/sockets.js
@@ -1,8 +1,14 @@
+// web socket Server connection 
 
 module.exports = function (io) {
   
   // web socket connection
   io.on('connection', socket => {
     console.log('new user connected')
+
+    socket.on('send message', function(data) {
+      io.sockets.emit('new message', data)
+    })
+
   })
 }
