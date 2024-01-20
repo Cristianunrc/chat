477c4afe7894285dfc9da376513744d380b911b8
Cristianunrc <cristiankhan23@gmail.com>
2024-01-20 18:29:36 -0300

    Connection with db MongoDB

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ const http = require('http')
 const path = require('path')
 const express = require('express')
 const { Server } = require('socket.io') // framework para conexiones en tiempo real
+const mongoose = require('mongoose')
 
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
+
+// db connection
+mongoose.connect('mongodb://localhost/chat-db')
+  .then(db => console.log('db is connected'))
+  .catch(err => console.log(err))
+
 const PORT = process.env.PORT || 3000
 
 require('./sockets')(io)
